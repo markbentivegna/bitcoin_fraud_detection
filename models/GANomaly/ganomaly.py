7e10291b1e8f38bd3338af7e89bd1e65fb7c9fc7
Mark Bentivegna <markbentivegna@gmail.com>
2023-11-02 20:05:41 -0400

    Adding ganomaly

---
+++ models/GANomaly/ganomaly.py
@@ -0,0 +1,24 @@
+import torch
+from torch import nn
+from models.GANomaly.generator import Generator
+from models.GANomaly.discriminator import Discriminator
+
+class GANomaly(nn.Module):
+    def __init__(self, input_dimension, latent_dimension, input_channels_count, features_count):
+        super().__init__()
+        self.generator = Generator(input_dimension, latent_dimension, input_channels_count, features_count)
+        self.discriminator = Discriminator(input_dimension, input_channels_count, features_count)
+        self.weights_init(self.generator)
+        self.weights_init(self.discriminator)
+        
+    def weights_init(self, module):
+        class_name = module.__class__.__name__
+        if class_name.find("Conv") != -1:
+            nn.init.normal_(module.weight.data, 0.0, 0.02)
+        elif class_name.find("BatchNorm") != -1:
+            nn.init.normal_(module.weight.data, 1.0, 0.02)
+            nn.init.constant_(module.bias.data, 0)
+
+    def forward(self, batch):
+        fake, latent_input, latent_output = self.generator(batch)
+        return batch, fake, latent_input, latent_output
